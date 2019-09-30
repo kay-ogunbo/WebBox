f89a6ae4f2904d908422b1cb319378adb0ce0c6d
Dan-Kode <55064257+Dan-Kode@users.noreply.github.com>
2019-09-30 16:37:56 +0100

    Added The 'Other' Panel and Category-Panel To The Homepage + Minor Tweak
    Added:
      - src/index.php
        - Added the Other-Panel(This Panel contains Buttons that go to other Pages such as the News Page and Contact-Page).
        - Added the Category-Panel(Clicking on one of the Links will lead the User to a Category on the Software-Page).
    
    Changed:
      - site.css
        - Changed the Drop-Down Shadow on Button Elements

---
+++ src/index.php
@@ -0,0 +1,102 @@
+<?php 
+/*
+ * Copyright belongs to: Oxygen-Software since <28/09/2019>
+ *
+ * This Program is Licensed under the OSPL(Oxygen-Software-Public-License) - V1.0.0(19/09/2019).
+ * To learn more about the License, please visit: https://oxygen-software.github.io/license.html
+*/
+/*
+ * WebBox
+ * @version 0.1.0
+ * @author Dan-Kode(Oxygen-Software)
+*/
+?>
+<!DOCTYPE html>
+<html>
+     <head>
+          <title>WebBox</title>
+          <meta charset="utf-8">
+          <!--Import all other Scripts here(That includes CSS and JS Files)-->
+          <link rel="stylesheet" type="text/css" href="assets/theme/css/index.css"> <!--Import the specific CSS File for this Page-->
+          <link rel="stylesheet" type="text/css" href="assets/theme/css/site.css"> <!--Import the Site-Wide CSS File-->
+     </head>
+     <body>
+          <h1 style="text-align: center; color: white; text-shadow: 0px 0px 5px black;">WebBox</h1>
+          <!--The Navigation-Menu for the Software. This Menu lets the User navigate through different Pages by clicking a Button.
+          
+          This Menu contains Buttons that link to:
+          - Homepage
+          - Software-Directory
+          - Login
+          - Register-->
+          <main>
+               <div class="Navigation-Menu">
+                    <!--Welcome the User-->
+                    <p style="font-size: 14px;">
+                         Welcome to WebBox, User! Please take your time to look around, settle in and see what the Software-Directory has to offer.
+                    </p>
+                    <img src="assets/theme/box_icons_v1/house.png" width="25" style="vertical-align: sub;"><a href="index.php"><button>Home</button></a>
+                    <img src="assets/theme/box_icons_v1/software.png" width="25" style="vertical-align: sub;"><a href="software.php"><button>Software</button></a>
+                    <label style="font-size: 20px;"> | </label>
+                    <img src="assets/theme/box_icons_v1/key.png" width="25" style="vertical-align: sub;"><button>Login</button>
+                    <img src="assets/theme/box_icons_v1/register.png" width="25" style="vertical-align: sub;"><button>Register</button>
+                    <br>
+                    <br>
+               </div>
+          </main>
+          <br>
+          <!--The 'Other' Panel. This Panel contains Buttons that link to other Pages. 
+          
+          These Pages are:
+          - News-Page
+          - About-Page
+          - Contact-Page
+          
+          THIS PANEL WILL NOT DISPLAY ON ANY OTHER PAGE.-->
+          <main>
+               <div class="Other-Panel">
+                    <h3>Other</h3>
+                    <hr>
+                    <!--The Nav-Menu for the Other-Panel.-->
+                    <a href="news.php"><button>News</button></a><br><br>
+                    <a href="about.php"><button>About</button></a><br><br>
+                    <a href="contact.php"><button>Contact Owner</button></a><br><br>
+               </div>
+          </main>
+          <br>
+          <!--The Category-Panel for the Homepage. This is a Homepage-Panel that displays a bunch of different Software-Categories.
+          
+          While this Panel is displayed on the Homepage, it is also seen on the Software-Page(At the very bottom of the Software-Page).-->
+          <main>
+               <div class="Category-Panel_Home">
+                    <h3>Categories</h3>
+                    <hr>
+                    <!--List of Software-Categories-->
+                    <ul id="Category-List_Home_left">
+                         <li><a href="">Utility</a></li>
+                         <li><a href="">Graphics</a></li>
+                         <li><a href="">Office & Workplace Tools</a></li>
+                    </ul>
+               </div>
+          </main>
+          <!--The Footer. This Footer contains Information about:
+          - The Software
+          - The Software Version
+          - Link to the Software's Website
+          - The Software's License
+          
+          NOTE TO USERS OF THE SOFTWARE: PLEASE DO NOT REMOVE ANYTHING FROM THIS FOOTER. DOING SO MAY CAUSE ISSUES WITH SUPPORT(E.G. REMOVING THE VERSION-NUMBER CAN STOP
+          PEOPLE FROM KNOWING WHICH VERSION YOU'RE USING AND STOPS THEM FROM BEING ABLE TO ENCOUNTER YOUR ERROR ON THAT VERSION).-->
+          <main>
+               <div class="Footer">
+                    <footer>
+                         <h5>This Website runs on WebBox | WebBox Version: 0.1.0(28/09/2019)</h5>
+                         <h5>
+                              <a href="https://webbox.github.io">Website</a> | 
+                              WebBox is Licensed under the OSPL. Learn more here: <a href="https://oxygen-software.github.io/license.html">Oxygen-Software: License</a>
+                         </h5>
+                    </footer>
+               </div>
+          </main>
+     </body>
+</html>
